f9a05bed4febded6f674bc32bddcd526f0ad9f17
Luke Simmons <lucasjsimmons@yahoo.com>
2025-07-07 10:24:01 -0400

    Add basic tests for Devise Users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
-    
+    email { "test123@email.com" }
+    password { "password" }
   end
 end
---
+++ spec/system/user_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+include Warden::Test::Helpers
+
+RSpec.describe 'users', type: :system do
+  let!(:user) { create(:user) }
+
+  context 'when user is not signed in' do
+    before do
+      visit '/'
+    end
+
+    it 'shows the login page' do
+      expect(page).to have_content("Log in")
+    end
+  end
+
+  context 'when user is signed in' do
+    before do
+      visit '/'
+      login_as user
+      visit '/'
+    end
+
+    it 'shows the games page' do
+      expect(page).to have_content("Games")
+    end
+  end
+end
