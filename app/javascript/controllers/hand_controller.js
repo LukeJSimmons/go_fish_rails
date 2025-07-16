1afdb90475e8471e58608ec8ee1596707318ec3c
Luke Simmons <lucasjsimmons@yahoo.com>
2025-07-16 10:20:19 -0400

    Add suit sorting

--- app/javascript/controllers/hand_controller.js
+++ app/javascript/controllers/hand_controller.js
@@ -3,16 +3,29 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="hand"
 export default class extends Controller {
   static targets = ['cards']
-  sort() {
-    const sorted_cards = [...this.cardsTarget.children].sort(this.sort_by_value)
+
+  sort_by_rank() {
+    const sorted_cards = [...this.cardsTarget.children].sort(this.by_value)
+    sorted_cards.forEach(node => this.cardsTarget.appendChild(node));
+  }
+
+  sort_by_suit() {
+    const sorted_cards = [...this.cardsTarget.children].sort(this.by_suit)
     sorted_cards.forEach(node => this.cardsTarget.appendChild(node));
   }
 
   private
 
-  sort_by_value(a,b) {
-    const a_value = a.classList[a.classList.length-1]
-    const b_value = b.classList[b.classList.length-1]
+  by_value(a,b) {
+    const a_value = a.dataset.value
+    const b_value = b.dataset.value
     return parseInt(a_value) > parseInt(b_value) ? 1 : -1
+  }
+
+  by_suit(a,b) {
+    const suits = ['H', 'D', 'S', 'C']
+    const a_suit = suits.indexOf(a.dataset.suit)
+    const b_suit = suits.indexOf(b.dataset.suit)
+    return a_suit > b_suit ? 1 : -1
   }
 }
