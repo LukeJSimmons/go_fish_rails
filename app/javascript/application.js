a7dcf5377a05a59f52a88d7fb5c75d86d144e64c
Luke Simmons <lucasjsimmons@yahoo.com>
2025-07-16 11:18:07 -0400

    Add confirm delete modal

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import './initializers/turbo_confirm.js'
+import './initializers/frame_missing_handler.js'
+import './initializers/before_morph_handler.js'
